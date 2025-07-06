8542a564ef2c01f1bd6b61a9256e4f107ecb10ba
AskaKolbaska <liza.v.tanke@yandex.ru>
2025-07-06 18:50:47 +0300

    final

--- Model.cs
+++ Model.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 using System.Threading.Tasks;
 
 namespace ToDo_App
@@ -15,32 +18,187 @@ namespace ToDo_App
         High // высокий
     }
 
-    // задача
-    public class Task
+    // действия для меню приложения
+    public enum MenuAction
     {
-        string _textTask; // текст задачи
-        bool _status; // статус выполнения
-        Priority _prioritet; // приоритет выполнения
+        Default, 
+        AddTask, // добавить задачу
+        RemoveTask, // удалить задачу
+        ChangeStatus, // изменить статус задачи
+        SaveFile, // сохранить в файл
+        SortList, // отсортировать по приоритету
+        CloseProgram // закрыть программу
+    }
 
-        public string TextTask { get { return _textTask; } set { _textTask = value; } }
-        public bool Status { get { return _status; } set { _status = value; } }
-        public Priority Prioritet { get { return _prioritet; } set { _prioritet = value; } }
+    // задача
+    public class ToDo_Task
+    {
+        public string TextTask { get; set; }
+        public bool Status { get; set; }
+        public Priority Prioritet { get; set; }
 
-        public Task() { 
+        public ToDo_Task() { 
             TextTask = string.Empty;
             Status = false;
             Prioritet = Priority.Default;
         }
 
-        public Task(string task) {
+        public ToDo_Task(string task) {
             TextTask = task;
             Status = false;
             Prioritet = Priority.Default;
         }
 
-        public Task(string task, Priority priority) : this(task)
+        public ToDo_Task(string task, Priority priority) : this(task)
         {
             Prioritet = priority;
+        }
+
+        public ToDo_Task(ToDo_Task source)
+        {
+            TextTask = source.TextTask;
+            Status = source.Status;
+            Prioritet = source.Prioritet;
+        }
+
+        public void ChangeStatus()
+        {
+            Status = !Status;
+        }
+    }
+
+    // список
+    public class ToDo_List
+    {
+        public List<ToDo_Task> ListNotCompleted { get; set; }
+        public List<ToDo_Task> ListCompleted { get; set; }
+
+        public ToDo_List() {
+            ListCompleted = new List<ToDo_Task>();
+            ListNotCompleted = new List<ToDo_Task>();
+        }
+
+        // кол-во элементов во ВСЕМ списке 
+        public int ListCount
+        {
+            get
+            {
+                int count = 0;
+                count += ListNotCompleted.Count;
+                count += ListCompleted.Count;
+
+                return count;
+            }
+        }
+
+        // добавить новую задачу
+        public void AddNewTask(ToDo_Task task) { 
+            ListNotCompleted.Add(task);
+        }
+
+        // удалить задачу
+        public void RemoveTask(int number) {
+            // если номер задачи входит в список невыполненных
+            if (number <= ListNotCompleted.Count) { 
+                ListNotCompleted.RemoveAt(number-1);
+            }
+            else
+            {
+                number = number - ListNotCompleted.Count;
+                ListCompleted.RemoveAt(number-1);
+            }
+        }
+
+        // изменить статус задачи
+        public void ChangeStatus(int number)
+        {
+            // если номер задачи входит в список невыполненных
+            if (number <= ListNotCompleted.Count)
+            {
+                var _task = new ToDo_Task(ListNotCompleted[number-1]); // копируем задачу
+                _task.ChangeStatus(); // меняем статус задачи
+                ListCompleted.Add(_task); // добавляем в выполненные
+                ListNotCompleted.RemoveAt(number - 1);
+            }
+            else
+            {
+                number = number - ListNotCompleted.Count; // номер в списке выполненных
+                var _task = new ToDo_Task(ListCompleted[number - 1]); // копируем задачу
+                _task.ChangeStatus(); // меняем статус задачи
+                ListCompleted.RemoveAt(number - ListNotCompleted.Count - 1);
+                ListNotCompleted.Add(_task); // добавляем в невыполненные
+                
+            }
+        }
+
+        public void SortLists()
+        {
+            ListCompleted = ListCompleted.OrderBy(x => x.Prioritet).ToList();
+            ListNotCompleted = ListNotCompleted.OrderBy(x => x.Prioritet).ToList();
+        }
+    }
+
+    // класс для работы с файлом
+    public static class FileHandler
+    {
+        public static void SaveToFile(string filePath, ToDo_List data)
+        {
+            try
+            {
+                var settings = new JsonSerializerSettings
+                {
+                    Formatting = Formatting.Indented,
+                    Converters = { new StringEnumConverter() }
+                };
+
+                string json = JsonConvert.SerializeObject(data, settings);
+                File.WriteAllText(filePath, json);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка сохранения: {ex.Message}");
+            }
+        }
+
+        public static ToDo_List LoadFromFile(string filePath)
+        {
+            try
+            {
+                if (!File.Exists(filePath))
+                {
+                    Console.WriteLine("Файл не существует. Создан новый список.");
+                    return new ToDo_List();
+                }
+
+                string json = File.ReadAllText(filePath);
+
+                var settings = new JsonSerializerSettings
+                {
+                    Converters = { new StringEnumConverter() },
+                    // Добавляем обработку отсутствующих значений
+                    MissingMemberHandling = MissingMemberHandling.Ignore,
+                    NullValueHandling = NullValueHandling.Ignore
+                };
+
+                var result = JsonConvert.DeserializeObject<ToDo_List>(json, settings);
+
+                // Гарантируем инициализацию списков
+                result.ListNotCompleted ??= new List<ToDo_Task>();
+                result.ListCompleted ??= new List<ToDo_Task>();
+
+                return result;
+            }
+            catch (JsonException ex)
+            {
+                Console.WriteLine($"Ошибка формата JSON: {ex.Message}");
+                Console.WriteLine("Создан новый список.");
+                return new ToDo_List();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Критическая ошибка: {ex.Message}");
+                return new ToDo_List();
+            }
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,11 +1,316 @@
-﻿using ToDo_App;
+﻿using System.ComponentModel.Design;
+using Spectre.Console;
+using System.Collections.Generic;
+using ToDo_App;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
+        ToDo_List list = new ToDo_List(); // список
+        list = FileHandler.LoadFromFile("tasks.json");
 
+        MenuAction act = MenuAction.Default; // действие с меню
+
+        Console.WriteLine("TO-DO LIST\n090304-РПИб-о23 Рыжкова Е.А.\n\n");
+
+        do
+        {
+            ShowList(list);
+            act = Menu();
+            switch (act)
+            {
+                case MenuAction.AddTask:
+                    {
+                        // Добавление задачи
+                        AddTask(list);
+                        break;
+                    }
+                case MenuAction.RemoveTask:
+                    {
+                        // Удаление задачи
+                        DeleteTask(list);
+                        break;
+                    }
+                case MenuAction.ChangeStatus:
+                    {
+                        // Изменить статус задачи
+                        ChangeStatusTask(list);
+                        break;
+                    }
+                case MenuAction.SaveFile:
+                    {
+                        // Сохранение в файл
+                        SaveToFile(list);
+                        break;
+                    }
+                case MenuAction.SortList:
+                    {
+                        // Вывести отсортированный список
+                        list.SortLists();
+                        break;
+                    }
+                case MenuAction.CloseProgram:
+                    {
+                        break;
+                    }
+            }
+        }
+        while (act != MenuAction.CloseProgram); // выполнять, пока не будет команды "Закрыть программу"
+
+        Console.ReadKey();
     }
 
+    // меню приложения
+    public static MenuAction Menu()
+    {
+        int otvet = 0; // ответ пользователя
+        MenuAction action = MenuAction.Default;
 
+        Console.WriteLine("Выберите действие:\n\n1. Добавить задачу\n" +
+            "2. Удалить задачу\n" +
+            "3. Сохранить список в файл\n" +
+            "4. Изменить статус задачи\n" +
+            "5. Отсортировать список по приоритету\n" +
+            "0. Закрыть программу\n");
+        Console.Write("Введите ответ: ");
+        int.TryParse(Console.ReadLine(), out otvet);
+
+        switch (otvet)
+        {
+            case 1:
+                {
+                    action = MenuAction.AddTask;
+                    break;
+                }
+            case 2: { 
+                    action = MenuAction.RemoveTask;
+                    break;
+                }
+            case 3:
+                {
+                    action = MenuAction.SaveFile;
+                    break;
+                }
+            case 4: {
+                    action = MenuAction.ChangeStatus;
+                    break;
+                }
+            case 5:
+                {
+                    action = MenuAction.SortList;
+                    break;
+                }
+            case 0:
+                {
+                    action = MenuAction.CloseProgram;
+                    break;
+                }
+        }
+
+        return action;
+    }
+
+    // Показать список
+    public static void ShowList(ToDo_List list)
+    {
+        // если список пуст
+        if (list.ListCount == 0)
+        {
+            Console.WriteLine("Список задач пуст!");
+        }
+        else {
+            var showTable = new Table()
+                .Border(TableBorder.Simple)
+                .Title("ToDo List");
+
+            showTable.AddColumn(new TableColumn("№").Centered())
+                .AddColumn(new TableColumn("Статус").Centered())
+                .AddColumn(new TableColumn("Задача"))
+                .AddColumn(new TableColumn("Приоритет"));
+
+            // сначала отобразим невыполненные задачи
+            int number = 0;
+            if(list.ListNotCompleted.Count != 0)
+            {
+                foreach (var item in list.ListNotCompleted)
+                {
+                    number++;
+                    string status = item.Status ? "[green]+[/]" : "[grey]-[/]";
+                    string prioritet = "";
+
+                    switch (item.Prioritet)
+                    {
+                        case Priority.Default:
+                            {
+                                prioritet = "";
+                                break;
+                            }
+                        case Priority.Low:
+                            {
+                                prioritet = "Низкий";
+                                break;
+                            }
+                        case Priority.Normal:
+                            {
+                                prioritet = "Средний";
+                                break;
+                            }
+                        case Priority.High:
+                            {
+                                prioritet = "Высокий";
+                                break;
+                            }
+                    }
+
+                    showTable.AddRow((number).ToString(), status, item.TextTask, prioritet);
+                }
+            }
+
+            // потом выполненные задачи
+            if (list.ListCompleted.Count != 0)
+            {
+                foreach (var item in list.ListCompleted)
+                {
+                    number++;
+                    string status = item.Status ? "[green]+[/]" : "[grey]-[/]";
+                    string prioritet = "";
+
+                    switch (item.Prioritet)
+                    {
+                        case Priority.Default:
+                            {
+                                prioritet = "";
+                                break;
+                            }
+                        case Priority.Low:
+                            {
+                                prioritet = "Низкий";
+                                break;
+                            }
+                        case Priority.Normal:
+                            {
+                                prioritet = "Средний";
+                                break;
+                            }
+                        case Priority.High:
+                            {
+                                prioritet = "Высокий";
+                                break;
+                            }
+                    }
+
+                    showTable.AddRow((number).ToString(), status, item.TextTask, prioritet);
+                }
+            }
+            
+            AnsiConsole.Write(showTable);
+        }
+    }
+
+    // Добавление в список задачу
+    public static void AddTask(ToDo_List list)
+    {
+        string textTask = null;
+        int otv = 0;
+        Priority prioritet = Priority.Default;
+
+        Console.Write("Введите текст задачи: ");
+        textTask = Console.ReadLine();
+
+        Console.WriteLine("Введите приоритет выполнения:\n0 - по умолчанию\n1 - низкий\n2 - средний\n3 - высокий");
+        int.TryParse(Console.ReadLine(), out otv);
+        switch (otv)
+        {
+            case 0:
+                {
+                    prioritet = Priority.Default;
+                    break;
+                }
+            case 1:
+                {
+                    prioritet = Priority.Low;
+                    break;
+                }
+            case 2:
+                {
+                    prioritet = Priority.Normal;
+                    break;
+                }
+            case 3:
+                {
+                    prioritet = Priority.High;
+                    break;
+                }
+        }
+
+        list.AddNewTask(new ToDo_Task(textTask, prioritet));
+    }
+
+    // Удалить задачу из списка
+    public static void DeleteTask(ToDo_List list)
+    {
+        // если список пуст
+        if (list.ListCount == 0)
+        {
+            Console.WriteLine("Список задач пуст!");
+        }
+        else
+        {
+            int otvet = 0;
+            Console.Write("Введите номер задачи: ");
+            if (int.TryParse(Console.ReadLine(), out otvet))
+            {
+                if (otvet > list.ListCount)
+                {
+                    Console.WriteLine("Этой задачи нет!");
+                }
+                else
+                {
+                    list.RemoveTask(otvet);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Не понимаю!");
+            }
+        }
+    }
+
+    // Изменить статус задачи
+    public static void ChangeStatusTask(ToDo_List list) {
+        // если список пуст
+        if (list.ListCount == 0)
+        {
+            Console.WriteLine("Список задач пуст!");
+        }
+        else
+        {
+            int otvet = 0;
+            Console.Write("Введите номер задачи: ");
+            if (int.TryParse(Console.ReadLine(),out otvet))
+            {
+                if (otvet > list.ListCount)
+                {
+                    Console.WriteLine("Этой задачи нет!");
+                }
+                else
+                {
+                    list.ChangeStatus(otvet);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Не понимаю!");
+            }
+
+            
+        }
+    }
+
+    public static void SaveToFile(ToDo_List list)
+    {
+        FileHandler.SaveToFile("tasks.json", list);
+        Console.WriteLine("Сохранение завершено!");
+    }
 }
